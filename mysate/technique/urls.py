0b51022d8c6180969c3eb0b439e11cc993814d33
StikSans <rukavcukroma@gmail.com>
2024-09-23 12:51:59 +0300

    practic 10

--- mysate/technique/urls.py
+++ mysate/technique/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home_index, product, about, product_detail
+from .views import home_index, product, about, product_detail, search_product
 
 urlpatterns = [
     path('home/', home_index, name='home'),
     path('product/', product, name='product'),
     path('abot/', about, name='about'  ),
     path('product/<int:pk>/', product_detail, name='product_detail'),
+    path('search_product/', search_product, name='search'),
 ]
--- mysate/technique/views.py
+++ mysate/technique/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpRequest
+from django.urls import reverse
 from .models.techique import Techique
+from django.db.models import Q
+
 
 
 
@@ -21,3 +24,13 @@ def about(req: HttpRequest):
 def product_detail(req: HttpRequest, pk: int):
     product = Techique.objects.get(pk=pk)
     return render(req, 'product_detail.html', {'product': product})
+
+
+def search_product(req: HttpRequest):
+    if req.method == 'GET':
+        search = req.GET['search']
+        product = Techique.objects.filter(Q(title__icontains=search) | Q(description__icontains=search))
+        return render(req, 'product.html', {'product': product})
+    
+    else:
+        return redirect('home')
